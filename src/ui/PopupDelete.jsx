98211d216b00ca085b019089bde220cbac7a193c
melroh629 <jw.roh629@gmail.com>
2024-04-18 16:04:03 +0900

    feat: 구독해지 팝업 컴포넌트 추가

---
+++ src/ui/PopupDelete.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export function PopupDelete({ pressName }) {
+	return (
+		<StyledWrapper>
+			<StyledTitle>
+				<strong>{pressName}</strong>을(를)
+				<br />
+				구독해지하시겠습니까?
+			</StyledTitle>
+			<StyledButtonWrapper>
+				<ButtonConfirm>예, 해지합니다</ButtonConfirm>
+				<ButtonClose>아니오</ButtonClose>
+			</StyledButtonWrapper>
+		</StyledWrapper>
+	);
+}
+const StyledWrapper = styled.div`
+	z-index: 10;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	width: 320px;
+	background-color: #fff;
+	border: 1px solid #d2dae0;
+`;
+const StyledTitle = styled.p`
+	padding: 24px;
+	font-size: 16px;
+	line-height: 22px;
+	text-align: center;
+`;
+const StyledButtonWrapper = styled.div`
+	display: flex;
+	background-color: #f5f7f9;
+	border-top: 1px solid #d2dae0;
+	button {
+		width: 50%;
+		padding: 13px 0;
+		&:hover {
+			font-weight: bold;
+		}
+	}
+`;
+const ButtonConfirm = styled.button`
+	border-right: 1px solid #d2dae0;
+`;
+const ButtonClose = styled.button`
+	color: #14212b;
+`;
